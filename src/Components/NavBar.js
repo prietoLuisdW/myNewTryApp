bcd0657d0259b577fc6ab2b02f3b726de402b6aa
Luis Prieto <lhprieto91@gmail.com>
2022-03-07 12:49:09 -0500

    NavBar v4.1 :Link Brand

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,20 +2,11 @@ import React from 'react'
 import { Link } from "react-router-dom"
 import { CartWidget } from './CartWidget'
 
-const categories = [
-    {id: 'capri', name: 'Capri'},
-    {id: 'cachetero', name: 'Short'},
-    {id: 'short', name: 'Cachetero'},
-    {id: 'batola', name: 'Batolas'},
-
-]
-
-
 export function NavBar() {
     return (
         <div className="navbar navbar-expand-md sticky-top navbar-dark text-white bg-black">
            <div className="container-fluid text-white">
-                <h3 type="button" className="navbar-brand navbar__brand hover-Black"as={Link} to="/">Evelu</h3>
+                <Link type="button" className="navbar-brand navbar__brand hover-Black" exact to="/">Evelu</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
