f803648e3f6790362ce88f049aa8aa09b9ac742a
Luis Prieto <lhprieto91@gmail.com>
2022-03-09 10:59:14 -0500

    Footer v1.1 :Normalizado

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,10 @@
 import waImg1 from '../pictures/wa.png' 
 export function Footer(){
     return(
-        <div>
-            <div className="bg-dark">
-                <p className='text-white'>Null Nadita Nada</p>                
+        <div className='py-2 bg-dark'>
+            <div className="ps-2 bg-dark">
+                <p className='fs_small mb-0 fw-light fc_grey'>Evelu Colombia</p>                
+                <p className='fs_small fw-light fc_grey'>Todos los derechos reservados</p>                
             </div>
             <a href="#" target="_blanck"><img src={waImg1} alt="Imagen whatsapp" className="wa"/></a>
         </div>
