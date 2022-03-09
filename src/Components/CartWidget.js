3c90c81c1818eb362a6b80266d1134ef697ad745
Luis Prieto <lhprieto91@gmail.com>
2022-03-08 21:49:54 -0500

    CartWidget :Ajuste Prop

--- src/Components/CartWidget.js
+++ src/Components/CartWidget.js
@@ -3,20 +3,12 @@ import cart from '../pictures/cart.svg'
 import { Link } from 'react-router-dom'
 
 
-export function CartWidget ({numeroUnidades}){
+export function CartWidget ({numOfItems}){
     // const { unidadesSeleccionadas } = useContext(Context)
     return(
         <Link type="Button" to="/cart" className="btn bg-secondary m-0 p-0 color-white ">
             <img src={cart} alt="Logo"/>
-            <span className="text-white badge bg-secondary"> {numeroUnidades}</span>
-                {/* {
-                    unidadesSeleccionadas>0?
-                    (
-                        <span className="text-white badge bg-secondary"> {unidadesSeleccionadas}</span>
-                    )
-                    :
-                    <></>
-                } */}
+            <span className="text-white badge bg-secondary"> {numOfItems}</span>
         </Link>
     )
 }
