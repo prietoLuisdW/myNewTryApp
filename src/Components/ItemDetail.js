825d93dd5416289d6a479fda2361d437f29f0acc
Luis Prieto <lhprieto91@gmail.com>
2022-03-08 21:18:53 -0500

    Item Detail v1 :Agree ItemCount

--- src/Components/ItemDetail.js
+++ src/Components/ItemDetail.js
@@ -1,15 +1,28 @@
+import ItemCount from "./ItemCount"
+
 export function ItemDetail({product}){
+    function OnAddCart(quantity){
+        console.log(quantity)
+    }
     return(
         <div className="contanier p-2 d-flex flex-wrap flex-sm-column justify-content-around">
-            <div clasName="w-50">
+            <div className="w-50">
                 <img src={product.imgUrl} alt={product.title} />
             </div>
-            <div className="">
-                <h5 className="text-capitalize">{product.id} : {product.categoryId}</h5>
-                <h5>{product.title}</h5>
-                <h5>{product.stock}</h5>
-                <h5>$ {product.precio} USD</h5>
-                <p>{product.descripcion}</p>
+            <div>
+                <div className="">
+                    <h5 className="text-capitalize">{product.id} : {product.categoryId}</h5>
+                    <h5>{product.title}</h5>
+                    <h5>{product.stock}</h5>
+                    <h5>$ {product.precio} USD</h5>
+                    <p>{product.descripcion}</p>
+                </div>
+                <div>
+                    <ItemCount
+                    initialValue={1}
+                    maxValue={product.value}
+                    onAdd={OnAddCart} />
+                </div>
             </div>
         </div>
     )
